65794c7b29ab030f115b0463af8e28568120c166
Andrew Minnich <aminnich3@gmail.com>
2019-08-08 04:29:05 -0400

    fix hover behavior on the rounded demo

--- demos/rounded.lua
+++ demos/rounded.lua
@@ -7,19 +7,19 @@ local beanMan = love.graphics.newImage 'bean man.png'
 local cornerRadius = 128
 
 function love.update(dt)
-	local targetCornerRadius = ui:isHovered 'imageContainer' and 4 or 128
+	local targetCornerRadius = ui:isHovered 'image' and 4 or 128
 	cornerRadius = cornerRadius + (targetCornerRadius - cornerRadius) * 10 * dt
 end
 
 function love.draw()
 	ui
 		:new 'rectangle'
-			:name 'imageContainer'
 			:beginChildren()
 				:new('image', beanMan)
+					:name 'image'
 					:set('scale', .5)
 					:center(400):middle(300)
-					:set('color', 1, 1, 1, ui:isHovered 'imageContainer' and 1 or .5)
+					:set('color', 1, 1, 1, ui:isHovered 'image' and 1 or .5)
 			:endChildren()
 			:wrap()
 			:clip()
