c445d149246be60993d89b37d8334b125d68f221
Andrew Minnich <aminnich3@gmail.com>
2019-08-31 19:29:22 -0400

    fillColor property

--- charm.lua
+++ charm.lua
@@ -17,24 +17,67 @@ function Element.base:new(x, y, width, height)
 	self.height = height or 0
 end
 
-function Element.base:draw()
-	love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
-end
-
 Element.rectangle = newElementClass(Element.base)
 
+Element.rectangle.set = {}
+
+function Element.rectangle.set:fillColor(r, g, b, a)
+	self.fillColor = self.fillColor or {}
+	if type(r) == 'table' then
+		for i = 1, 4 do self.fillColor[i] = r[i] end
+	else
+		self.fillColor[1] = r
+		self.fillColor[2] = g
+		self.fillColor[3] = b
+		self.fillColor[4] = a
+	end
+end
+
+function Element.rectangle:draw()
+	love.graphics.push 'all'
+	if self.fillColor and #self.fillColor > 1 then
+		love.graphics.setColor(self.fillColor)
+		love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+	end
+	love.graphics.pop()
+end
+
 local Ui = {}
-Ui.__index = Ui
+
+function Ui:__index(k)
+	if Ui[k] then return Ui[k] end
+	self._propertyCache[k] = self._propertyCache[k] or function(_, ...)
+		return self:set(k, ...)
+	end
+	return self._propertyCache[k]
+end
 
 function Ui:_getElementClass(className)
 	if type(className) == 'table' then return className end
 	return Element[className]
 end
 
+function Ui:select(element)
+	self._selectedElement = element
+end
+
+function Ui:_clearElement(element)
+	for key, value in pairs(element) do
+		if type(value) == 'table' then
+			for nestedKey in pairs(value) do
+				value[nestedKey] = nil
+			end
+		else
+			element[key] = nil
+		end
+	end
+end
+
 function Ui:new(className, ...)
 	local element
 	for _, e in ipairs(self._elementPool) do
 		if not e._used then
+			self:_clearElement(e)
 			element = e
 			break
 		end
@@ -47,6 +90,15 @@ function Ui:new(className, ...)
 	setmetatable(element, self:_getElementClass(className))
 	if element.new then element:new(...) end
 	table.insert(self._elements, element)
+	self:select(element)
+	return self
+end
+
+function Ui:set(property, ...)
+	local element = self._selectedElement
+	if element.set and element.set[property] then
+		element.set[property](element, ...)
+	end
 	return self
 end
 
@@ -57,8 +109,8 @@ function Ui:_draw()
 end
 
 function Ui:_finish()
-	for elementIndex = #self._elements, 1, -1 do
-		self._elements[elementIndex] = nil
+	for i = #self._elements, 1, -1 do
+		self._elements[i] = nil
 	end
 	for _, element in ipairs(self._elementPool) do
 		element._used = false
@@ -74,6 +126,8 @@ function charm.new()
 	return setmetatable({
 		_elements = {},
 		_elementPool = {},
+		_selectedElement = nil,
+		_propertyCache = {},
 	}, Ui)
 end
 
--- main.lua
+++ main.lua
@@ -1,8 +1,9 @@
 local ui = require 'charm'.new()
 
 function love.draw()
+	ui:new('rectangle', 50, 50, 100, 150)
 	if love.keyboard.isDown 'space' then
-		ui:new('rectangle', 50, 50, 100, 150)
+		ui:fillColor(1, 0, 0)
 	end
 	love.graphics.print('Number of elements: ' .. #ui._elements, 0, 16)
 	ui:draw()
