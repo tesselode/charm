d19d8db75f2bace9d2d59f570f299a7b0e25094b
Andrew Minnich <aminnich3@gmail.com>
2019-12-15 16:42:06 -0500

    fresh start
    i'm going to try rewriting charm to be a library purely for rendering - no mouse events or state.
    
    i've been trying to make charm a low-level GUI lib as well as a layout lib because i feel like it's a missed opportunity if i don't. but this isn't actually a use case that i'll use, and i think it shows in the design. the library is overcomplicated and doesn't solve the problems it sets out to.
    
    i hope that by rewriting charm with a more singular focus, it'll benefit in design and functionality.

--- main.lua
+++
@@ -1,8 +0,0 @@
-function love.load(arg)
-	require('demos.' .. arg[1])
-	local oldDraw = love.draw
-	function love.draw()
-		if oldDraw then oldDraw() end
-		love.graphics.print(string.format('Memory usage: %ikb', collectgarbage 'count'))
-	end
-end
--- todo.md
+++
@@ -1,3 +0,0 @@
-Todo:
-- Remove an element's state once the element stops existing
-- Split up the library into multiple files (?)
