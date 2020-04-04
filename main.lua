ecebfc238a97767e74d69ba280f926ad1669bd51
Andrew Minnich <aminnich3@gmail.com>
2020-04-04 01:21:59 -0400

    i have Ideas

--- main.lua
+++
@@ -1,8 +0,0 @@
-function love.load(arguments)
-	require('demos.' .. arguments[1])
-	local oldDraw = love.draw
-	function love.draw()
-		if oldDraw then oldDraw() end
-		love.graphics.print(string.format('Memory usage: %ikb', collectgarbage 'count'))
-	end
-end
